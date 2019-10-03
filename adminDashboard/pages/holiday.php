27f9365d2ca55005e680516403d2c357c05e157f
JayanthNarla <47258705+JayanthNarla@users.noreply.github.com>
2019-10-03 20:29:50 +0530

    code cleanup

--- adminDashboard/pages/holiday.php
+++ adminDashboard/pages/holiday.php
@@ -254,50 +254,7 @@ else
     ?>
 </div>
 
-	<!-- <form method="post" action="server.php" >
-	<input type="hidden" name="id" value="<?php echo $id; ?>">
-		<div class="input-group">
-			<label>reason</label>
-			<input type="text" name="name" value="<?php echo $name; ?>">
-
-		</div>
-		<div class="input-group">
-			<label>date</label>
-			<input type="date" name="date" value="<?php echo $date; ?>">
-
-		</div>
-		<div class="input-group">
-			<?php if ($update == true): ?>
-	<button class="btn" type="submit" name="update" style="background: #556B2F;" >update</button>
-<?php else: ?>
-	<button class="btn" type="submit" name="save" >Save</button>
-<?php endif ?>
-		</div>
-	</form> -->
-	<!-- <form>
-<table>
-	<thead>
-		<tr>
-			<th>Holiday</th>
-			<th>Date</th>
-			<th colspan="2">Action</th>
-		</tr>
-	</thead>
-	
-	<?php while ($row = mysqli_fetch_array($results)) { ?>
-		<tr>
-			<td><?php echo $row['reason']; ?></td>
-			<td><?php echo $row['date']; ?></td>
-			<td>
-				<a href="holiday.php?edit=<?php echo $row['id']; ?>" class="edit_btn" >Edit</a>
-			</td>
-			<td>
-				<a href="server.php?del=<?php echo $row['id']; ?>" class="del_btn">Delete</a>
-			</td>
-		</tr>
-	<?php } ?>
-</table></!--> 
-        </div>
+	</div>
         <!-- /#page-wrapper -->
 
     </div>
